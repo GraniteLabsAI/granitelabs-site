6a04e7701e8c3ac2780951b7e512476857ecd4a6
GraniteLabsAI <stanroth1@gmail.com>
2026-03-17 06:43:30 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,3 +1,53 @@
+ // @ts-check
+import {themes as prismThemes} from 'prism-react-renderer';
+
+const config = {
+  title: 'Granite Labs',
+  tagline: 'Searchable Deployment Documentation',
+  favicon: 'img/favicon.ico',
+
+  url: 'https://granitelabsai.github.io',
+  baseUrl: '/granitelabs-site/',
+
+  organizationName: 'GraniteLabsAI',
+  projectName: 'granitelabs-site',
+
+  onBrokenLinks: 'throw',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: './sidebars.js',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      },
+    ],
+  ],
+
+  plugins: [
+    [
+      require.resolve('@easyops-cn/docusaurus-search-local'),
+      {
+        hashed: true,
+        language: ['en'],
+        indexDocs: true,
+        indexBlog: false,
+        indexPages: false,
+      },
+    ],
+  ],
+
   themeConfig: {
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
@@ -26,3 +76,6 @@
       darkTheme: prismThemes.dracula,
     },
   },
+};
+
+export default config;
