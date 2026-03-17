61350eb38fed6f13839893e25f34acf957ceb6e8
GraniteLabsAI <stanroth1@gmail.com>
2026-03-17 06:40:15 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,64 +1,13 @@
-// @ts-check
-import {themes as prismThemes} from 'prism-react-renderer';
-
-const config = {
-  title: 'Granite Labs',
-  tagline: 'Searchable Deployment Documentation',
-  favicon: 'img/favicon.ico',
-
-  url: 'https://granitelabsai.github.io',
-  baseUrl: '/granitelabs-site/',
-
-  organizationName: 'GraniteLabsAI',
-  projectName: 'granitelabs-site',
-
-  onBrokenLinks: 'throw',
-
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
-
-  presets: [
-    [
-      'classic',
-      {
-        docs: {
-          routeBasePath: '/',
-          sidebarPath: './sidebars.js',
-        },
-        blog: false,
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      },
-    ],
-  ],
-
-  plugins: [
-    [
-      require.resolve('@easyops-cn/docusaurus-search-local'),
-      {
-        hashed: true,
-        language: ['en'],
-        indexDocs: true,
-        indexBlog: false,
-        indexPages: false,
-      },
-    ],
-  ],
-
   themeConfig: {
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-  title: 'Granite Labs',
-  logo: {
-    alt: 'Granite Labs Logo',
-    src: 'img/cube_1024_master.png',
-    height: 32,
-    width: 32,
-  },
-},
+      title: 'Granite Labs',
+      logo: {
+        alt: 'Granite Labs Logo',
+        src: 'img/cube_1024_master.png',
+        height: 32,
+        width: 32,
+      },
       items: [
         {
           href: 'https://github.com/GraniteLabsAI/granitelabs-site',
@@ -77,6 +26,3 @@ const config = {
       darkTheme: prismThemes.dracula,
     },
   },
-};
-
-export default config;
